9e4f7c6a74ef13c1e34a93115db7549ae6e5e87b
Colin <colin.heggli@bbz-sh.ch>
2023-11-15 14:18:39 +0100

    Init

---
+++ Minesweeper/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Minesweeper
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txt_size = New System.Windows.Forms.TextBox()
+        Me.btn_start = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_flages_left = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txt_size
+        '
+        Me.txt_size.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_size.Location = New System.Drawing.Point(50, 12)
+        Me.txt_size.Name = "txt_size"
+        Me.txt_size.Size = New System.Drawing.Size(126, 38)
+        Me.txt_size.TabIndex = 1
+        '
+        'btn_start
+        '
+        Me.btn_start.Location = New System.Drawing.Point(182, 12)
+        Me.btn_start.Name = "btn_start"
+        Me.btn_start.Size = New System.Drawing.Size(65, 38)
+        Me.btn_start.TabIndex = 2
+        Me.btn_start.Text = "Start"
+        Me.btn_start.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(253, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(65, 38)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Flages: "
+        '
+        'lbl_flages_left
+        '
+        Me.lbl_flages_left.AutoSize = True
+        Me.lbl_flages_left.Location = New System.Drawing.Point(102, 63)
+        Me.lbl_flages_left.Name = "lbl_flages_left"
+        Me.lbl_flages_left.Size = New System.Drawing.Size(0, 16)
+        Me.lbl_flages_left.TabIndex = 5
+        '
+        'Minesweeper
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 537)
+        Me.Controls.Add(Me.lbl_flages_left)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btn_start)
+        Me.Controls.Add(Me.txt_size)
+        Me.Name = "Minesweeper"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_size As TextBox
+    Friend WithEvents btn_start As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl_flages_left As Label
+End Class
---
+++ Minesweeper/Form1.vb
@@ -0,0 +1,283 @@
+﻿Public Class Minesweeper
+    Public MinesFound As Integer = 0
+    Public Mines As Integer = 0
+    Public FieldSize As Integer = 12
+    Public FlagesPlaces As Integer = 0
+    Public Field(30, 30) As Integer
+    Public FieldsClicked As Integer = 0
+    Public ClickedMines As Integer = 0
+    Public WrongClicks As Integer = 0
+    Public FirstClick As Boolean = True
+
+
+    Private Sub Button_Click(sender As Object, e As EventArgs)
+
+        Dim Position As String = sender.tag
+        Dim Pos_y As Integer = CInt(Position.Split("_")(0))
+        Dim Pos_x As Integer = CInt(Position.Split("_")(1))
+        Dim Button As Button = DirectCast(sender, Button)
+
+        If FirstClick Then
+            Fill_Field_Matrix(Pos_y, Pos_x)
+            FirstClick = False
+        End If
+        'MsgBox("Y:" & Pos_y & " X: " & Pos_x & vbNewLine & "Value: " & Field(Pos_y, Pos_x))
+
+        If Button.Text = "F" Then
+            Exit Sub
+        End If
+        If Field(Pos_y, Pos_x) >= 9 Then
+            MsgBox("You Lose")
+            Reveal_Mines()
+            Exit Sub
+        Else
+            Button.Text = Field(Pos_y, Pos_x)
+            Button.Enabled = False
+        End If
+        FieldsClicked += 1
+    End Sub
+
+    Private Sub Button_MouseClick(sender As Object, e As MouseEventArgs)
+        ' Right-click event handling logic
+        Dim Button As Button = DirectCast(sender, Button)
+        Dim Position As String = sender.tag
+        Dim Pos_y As Integer = CInt(Position.Split("_")(0))
+        Dim Pos_x As Integer = CInt(Position.Split("_")(1))
+        If FirstClick Then
+            Fill_Field_Matrix(Pos_y, Pos_x)
+            FirstClick = False
+        End If
+
+        If e.Button = MouseButtons.Right Then
+            If Button.Text = "" AndAlso FlagesPlaces < Mines Then
+                Button.Text = "F"
+                Button.ForeColor = Color.Red
+                FlagesPlaces += 1
+                FieldsClicked += 1
+                If Field(Pos_y, Pos_x) >= 9 Then
+                    MinesFound += 1
+                End If
+            ElseIf Button.Text = "F" Then
+                Button.Text = ""
+                Button.ForeColor = Color.Black
+                FlagesPlaces -= 1
+                FieldsClicked -= 1
+                If Field(Pos_y, Pos_x) >= 9 Then
+                    MinesFound -= 1
+                End If
+            End If
+            _lbl_flages_left.Text = Mines - FlagesPlaces
+        End If
+
+        If MinesFound = Mines And (FieldSize * FieldSize) = FieldsClicked Then
+            MsgBox("You Win")
+
+        End If
+    End Sub
+
+    Private Sub DeleteButtonMatrix()
+        Dim buttonsToDelete As New List(Of Control)
+
+        ' Collect buttons with a specific naming pattern
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is Button AndAlso ctrl.Name.StartsWith("fld_") Then
+                buttonsToDelete.Add(ctrl)
+            End If
+        Next
+
+        ' Delete the collected buttons
+        For Each button As Control In buttonsToDelete
+            Me.Controls.Remove(button)
+            button.Dispose() ' Optional: Dispose to release resources
+        Next
+    End Sub
+
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+        If Integer.TryParse(_txt_size.Text, Nothing) Then
+            FieldSize = CInt(_txt_size.Text)
+            'MsgBox("Field Size: " & FieldSize)
+            If FieldSize > 30 Then
+                MsgBox("Please enter a valid Field Size (max 30)")
+                Exit Sub
+            End If
+        Else
+            MsgBox("Please enter a valid Field Size (max 30)")
+            Exit Sub
+        End If
+        DeleteButtonMatrix()
+        Build_Fieled()
+
+
+        _lbl_flages_left.Text = Mines - FlagesPlaces
+    End Sub
+
+
+    Private Sub Build_Fieled()
+        Const buttonWidth As Integer = 25
+        Const buttonHeight As Integer = 25
+        Const spacing As Integer = 2
+
+        For i As Integer = 0 To FieldSize - 1
+            For j As Integer = 0 To FieldSize - 1
+                Dim newButton As New Button()
+
+                ' Set properties for the button
+                newButton.Width = buttonWidth
+                newButton.Height = buttonHeight
+                newButton.Left = j * (buttonWidth + spacing) + 50
+                newButton.Top = i * (buttonHeight + spacing) + 100
+                newButton.Text = ""
+                newButton.Tag = $"{i}_{j}"
+                newButton.Name = $"fld_{i}_{j}"
+
+                ' Add click event handler if needed
+                AddHandler newButton.Click, AddressOf Button_Click
+                AddHandler newButton.MouseUp, AddressOf Button_MouseClick
+
+                ' Add the button to the form
+                Me.Controls.Add(newButton)
+            Next
+        Next
+    End Sub
+
+    Private Sub Fill_Field_Matrix(y, x)
+        Mines = 0
+        MinesFound = 0
+        FlagesPlaces = 0
+        Dim rand As New Random()
+        Dim threshold As Double = 0.3 ' 30% chance for 9
+        For i As Integer = 0 To FieldSize - 1
+            For j As Integer = 0 To FieldSize - 1
+                Field(i, j) = 0
+            Next
+        Next
+
+        For i As Integer = 0 To FieldSize - 1
+            For j As Integer = 0 To FieldSize - 1
+                If i = y AndAlso j = x Then
+                    Exit For
+                End If                ' Generate a random number between 0 and 1
+                Dim randomValue As Double = rand.NextDouble()
+                ' Check if the random number falls below the threshold
+                If randomValue < threshold Then
+                    ' Assign 9 if the random number is below the threshold
+                    Field(i, j) = 9
+                    Mines += 1
+                    check_mine_radius(i, j)
+                    'Else
+                    ' Assign 0 if the random number is above the threshold
+                    '    Field(i, j) = 0
+                End If
+            Next
+        Next
+        'MsgBox("Mines: " & Mines)
+        _lbl_flages_left.Text = Mines
+        'PrintArrayInMessageBox()
+    End Sub
+
+    Private Sub PrintArrayInMessageBox()
+        Dim output As String = ""
+
+        For i As Integer = 0 To Field.GetUpperBound(0) ' Iterate through rows
+            Dim rowString As String = ""
+
+            For j As Integer = 0 To Field.GetUpperBound(1) ' Iterate through columns
+                rowString &= Field(i, j).ToString() & " " ' Add each element to the row string
+            Next
+
+            output &= rowString.Trim() & vbCrLf ' Add the row to the output string with a newline
+        Next
+
+        MessageBox.Show(output) ' Display the whole array in a message box
+    End Sub
+
+    Private Sub check_mine_radius(x, y)
+        Try
+            If Integer.TryParse(Field(x - 1, y), Nothing) Then
+                Field(x - 1, y) = CInt(Field(x - 1, y)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x - 1, y - 1), Nothing) Then
+                Field(x - 1, y - 1) = CInt(Field(x - 1, y - 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x, y - 1), Nothing) Then
+                Field(x, y - 1) = CInt(Field(x, y - 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x + 1, y), Nothing) Then
+                Field(x + 1, y) = CInt(Field(x + 1, y)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x + 1, y + 1), Nothing) Then
+                Field(x + 1, y + 1) = CInt(Field(x + 1, y + 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x, y + 1), Nothing) Then
+                Field(x, y + 1) = CInt(Field(x, y + 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x - 1, y + 1), Nothing) Then
+                Field(x - 1, y + 1) = CInt(Field(x - 1, y + 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Try
+            If Integer.TryParse(Field(x + 1, y - 1), Nothing) Then
+                Field(x + 1, y - 1) = CInt(Field(x + 1, y - 1)) + 1
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Private Sub Reveal_Mines()
+        For i As Integer = 0 To FieldSize - 1
+            For j As Integer = 0 To FieldSize - 1
+                Dim button As Button = DirectCast(Me.Controls("fld_" & i & "_" & j), Button)
+                button.Enabled = False
+                If Field(i, j) >= 9 Then
+                    'get button with name fld_(i)_(j)
+                    button.Text = "X"
+                    button.ForeColor = Color.Red
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DeleteButtonMatrix()
+        For i As Integer = 0 To FieldSize - 1
+            For j As Integer = 0 To FieldSize - 1
+                Field(i, j) = 0
+            Next
+        Next
+    End Sub
+End Class
